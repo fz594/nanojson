837518fab367e814600ecd9b81203e96fb17ae8e
wb9688 <wb9688@users.noreply.github.com>
2020-04-16 11:18:29 +0200

    Support keywords as keys

--- src/main/java/com/grack/nanojson/JsonParser.java
+++ src/main/java/com/grack/nanojson/JsonParser.java
@@ -188,8 +188,18 @@ public final class JsonParser {
 				JsonObject map = new JsonObject();
 				if (advanceToken() != JsonTokener.TOKEN_OBJECT_END)
 					while (true) {
-						if (token != JsonTokener.TOKEN_STRING && token != JsonTokener.TOKEN_SEMI_STRING)
+						switch (token) {
+						case JsonTokener.TOKEN_NULL:
+						case JsonTokener.TOKEN_TRUE:
+						case JsonTokener.TOKEN_FALSE:
+							value = value.toString();
+							break;
+						case JsonTokener.TOKEN_STRING:
+                        case JsonTokener.TOKEN_SEMI_STRING:
+							break;
+                        default:
 							throw tokener.createParseException(null, "Expected STRING, got " + token, true);
+						}
 						String key = (String)value;
 						if (token == JsonTokener.TOKEN_SEMI_STRING) {
 							if (advanceTokenOld() != JsonTokener.TOKEN_COLON)
@@ -265,8 +275,18 @@ public final class JsonParser {
 			JsonObject map = new JsonObject();
 			if (advanceToken() != JsonTokener.TOKEN_OBJECT_END)
 				while (true) {
-					if (token != JsonTokener.TOKEN_STRING && token != JsonTokener.TOKEN_SEMI_STRING)
+					switch (token) {
+					case JsonTokener.TOKEN_NULL:
+					case JsonTokener.TOKEN_TRUE:
+					case JsonTokener.TOKEN_FALSE:
+						value = value.toString();
+						break;
+					case JsonTokener.TOKEN_STRING:
+					case JsonTokener.TOKEN_SEMI_STRING:
+						break;
+					default:
 						throw tokener.createParseException(null, "Expected STRING, got " + token, true);
+					}
 					String key = (String)value;
 					if (token == JsonTokener.TOKEN_SEMI_STRING) {
 						if (advanceTokenOld() != JsonTokener.TOKEN_COLON)
